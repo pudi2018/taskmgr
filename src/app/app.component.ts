8345b9dba0d3645af55a0d1410b467e2daa52ecd
pudi2018 <pudi2018@gmail.com>
2018-10-14 21:42:04 +0800

    测试

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
       state('red', style({'background-color': 'red', 'height': '100px', 'transform': 'translateY(100%)'})),
       transition('green => red', animate('.8s cubic-bezier(.11, .67, 1, .58)')), // http://cubic-bezier.com/#.17,.67,.83,.67 动画效果链接
       transition('red => green', animate('.8s ease-out')),
+
     ])
   ]
 })
